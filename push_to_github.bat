aa0811058ffb947ba2320e10fa3038ee0786b7e9
afyonluoglu <afyonluoglu@gmail.com>
2025-08-17 13:39:11 +0300

    Güncellemeler yapıldı

--- push_to_github.bat
+++
@@ -1,8 +0,0 @@
-git rm -r --cached src/saves/
-git add .
-git commit -m "Güncellemeler yapıldı"
-git push
-#gitignore’da tanımlanmasına rağmen push edilenleri kaldır
-git rm --cached -r  src/data/__pycache__ src/game/__pycache__ src/utils/__pycache__
-git commit -m "Remove __pycache__ directories from repository completely"
-git push
